73645b56e1a5420c41d8870b8934906d2613e647
zizzu <39168861+zizzu0@users.noreply.github.com>
2020-02-23 23:03:24 +0000

    Update ip2location.py

--- ip2location.py
+++ ip2location.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Convert ip2location lite database to human readable
-# https://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP
+# https://lite.ip2location.com/file-download 
 
 from socket import inet_ntoa
 from struct import pack
@@ -8,8 +8,8 @@ from struct import pack
 def long2ip(ip):
         return inet_ntoa(pack("!L", ip))
 
-fin = open('IP2LOCATION-LITE-DB1.CSV/IP2LOCATION-LITE-DB1.CSV','r')
-fout = open('IP2LOCATION-LITE-DB1.CSV/IP2LOCATION-LITE-DB1.txt','w')
+fin = open('IP2LOCATION-LITE-DB3.CSV','r')
+fout = open('IP2LOCATION-LITE-DB3.txt','w')
 
 for line in fin:
         tokens = line.replace('"','').split(',')
@@ -17,9 +17,11 @@ for line in fin:
         stop = int(tokens[1])
         code = tokens[2]
         country = tokens[3]
+        region = tokens[4]
+        city = tokens[5]
 
-        fout.write('%s %s %s %s' %
-        (long2ip(start),long2ip(stop),code, country)
+        fout.write('%s %s %s %s %s %s' %
+        (long2ip(start),long2ip(stop),code, country, region, city)
         )
 
 fin.close()
